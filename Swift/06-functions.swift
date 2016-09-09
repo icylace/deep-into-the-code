68f8bd93fb1614aace05290ad0f9bff0698869f4
Ron Martinez <ramartin.net@gmail.com>
2016-09-09 03:18:05 -0400

    Add more Swift stuff

--- Swift/06-functions.swift
+++ Swift/06-functions.swift
@@ -1,12 +1,319 @@
-// Defining and Calling Functions
-// Function Parameters and Return Values
-// Function Parameter Names
-// Function Types
-// Nested Functions
+// =============================================================================
+//  Functions
+//  https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Functions.html
+// =============================================================================
 
 
+// -----------------------------------------------------------------------------
+//  Defining and Calling Functions
+// -----------------------------------------------------------------------------
 
-
-
-func aaa(a: String..., b: String) {
+// Define a function called `sayHi` which accepts a string as its sole input
+// and that also returns a string.
+func sayHi(name: String) -> String {
+  let greeting = "Hello, \(name)!"
+  return greeting
 }
+
+// Call `sayHi` by passing it a string value that has the label `name`.
+let a = sayHi(name: "Anna")
+assert(a == "Hello, Anna!")
+
+
+// -----------------------------------------------------------------------------
+//  Function Parameters and Return Values
+// -----------------------------------------------------------------------------
+
+// A function that takes no parameters.
+func sayHi() -> String {
+  return "hi"
+}
+
+let d = sayHi()
+assert(d == "hi")
+
+// -----------------------------------------------------------------------------
+
+// A function that takes a couple parameters.
+func sayHi(name: String, alreadyGreeted: Bool) -> String {
+  if alreadyGreeted {
+    return "Hello again, \(name)!"
+  } else {
+    return sayHi(name: name)
+  }
+}
+
+let e = sayHi(name: "Tim", alreadyGreeted: true)
+assert(e == "Hello again, Tim!")
+
+// -----------------------------------------------------------------------------
+
+// A function that doesn't return anything.
+func sayBye(name: String) {
+  print("Goodbye, \(name)!")
+}
+
+sayBye(name: "Dave")
+// Prints "Goodbye, Dave!"
+
+// -----------------------------------------------------------------------------
+
+// Strictly speaking, the `sayBye` function does still return a value, even
+// though no return value is defined.  Functions without a defined return
+// return a special value of type `Void`.  This is simply an empty tuple,
+// in effect a tuple with zero elements, which can be written as `()`.
+
+func sayByeAgain(name: String) -> Void {
+  print("Goodbye again, \(name)!")
+}
+
+sayByeAgain(name: "Dave")
+// Prints "Goodbye again, Dave!"
+
+func sayFinalBye(name: String) -> () {
+  print("Final goodbye, \(name)!")
+}
+
+sayFinalBye(name: "Dave")
+// Prints "Final goodbye, Dave!"
+
+// -----------------------------------------------------------------------------
+
+// A function's return value can be ignored.
+func printAndCount(text: String) -> Int {
+  print(text)
+  return text.characters.count
+}
+func printWithoutCounting(text: String) {
+  printAndCount(text: text)
+}
+printAndCount(text: "hello, world")
+// Prints "hello, world" and returns a value of 12.
+printWithoutCounting(text: "hello, world")
+// Prints "hello, world" but does not return a value.
+
+// Return values can be ignored, but a function with a defined return type
+// must always return a value.
+
+// -----------------------------------------------------------------------------
+
+// A tuple can be used to emulate multiple simultaneous return values.
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+  var curMin = array[0]
+  var curMax = array[0]
+  for value in array[1..<array.count] {
+    if value < curMin {
+      curMin = value
+    } else if value > curMax {
+      curMax = value
+    }
+  }
+  return (curMin, curMax)
+}
+
+// Because the tuple's member values are named as part of the function's return
+// type, they can be accessed with dot syntax.
+let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
+assert(bounds.min == -6)
+assert(bounds.max == 109)
+
+// -----------------------------------------------------------------------------
+
+// An optional tuple type such as `(Int, Int)?` is different from a tuple
+// that contains optional types such as `(Int?, Int?)`.  With an optional
+// tuple type, the entire tuple is optional, not just each individual
+// value within the tuple.
+
+func minMax2(array: [Int]) -> (min: Int, max: Int)? {
+  if array.isEmpty { return nil }
+  var curMin = array[0]
+  var curMax = array[0]
+  for value in array[1..<array.count] {
+    if value < curMin {
+      curMin = value
+    } else if value > curMax {
+      curMax = value
+    }
+  }
+  return (curMin, curMax)
+}
+
+if let bounds = minMax2(array: [8, -6, 2, 109, 3, 71]) {
+  assert(bounds.min == -6)
+  assert(bounds.max == 109)
+}
+
+
+// -----------------------------------------------------------------------------
+//  Function Parameter Names
+// -----------------------------------------------------------------------------
+
+// A function that takes a single parameter.
+func f1(parameter: Int) {
+  // Within the function, `parameter` is used to refer to the parameter value.
+  assert(parameter == 1)
+}
+
+// By default the parameter name must be used as a label for
+// the argument passed to the function.
+f1(parameter: 1)
+
+// -----------------------------------------------------------------------------
+
+// Parameters can have custom external parameter names.
+func f2(externalParameterName parameter: Int) {
+  // Within the function, `parameter` is used
+  // instead of `externalParameterName`.
+  assert(parameter == 1)
+}
+
+// The external parameter name must be used as a label for
+// the argument passed to the function.
+f2(externalParameterName: 1)
+
+// -----------------------------------------------------------------------------
+
+// If a parameter's external name is an underscore then
+// it's not labelled in calls to the function.
+func f3(_ parameter: Int) {
+  assert(parameter == 1)
+}
+
+// The parameter label is prohibited.
+f3(1)
+
+// -----------------------------------------------------------------------------
+
+// Parameters can be assigned a default value.
+func f4(parameter: Int = 2) {
+  // If no valid arguments are passed to the function call then
+  // the value of `parameter` will be 2.
+  assert(parameter % 2 == 0)
+}
+
+f4(parameter: 4)        // `parameter` will be 4.
+f4()        // `parameter` will be 2.  Note the label is not needed here.
+
+// STYLE:
+// Place parameters with default values at the end of a function's parameter
+// list.  This ensures that all calls to the function use the same order for
+// their nondefault arguments, and makes it clear that the same function is
+// being called in each case.
+
+// -----------------------------------------------------------------------------
+
+// A variadic parameter accepts zero or more values of a specified type.
+// A function may have at most one variadic parameter.
+func f5(numbers: Double...) -> Double {
+  var total: Double = 1
+  for number in numbers {
+    total *= number
+  }
+  return total
+}
+
+let r5 = f5(numbers: 2.35, 6.7, 55.0)
+assert(r5 == 865.975)
+
+// -----------------------------------------------------------------------------
+
+// Function parameters are constants by default.  Parameters that can be
+// modified are in-out parameters.
+
+func f6(x: inout Int) {
+  x *= 2
+}
+
+var r6 = 4
+// The ampersand is needed because we're passing in a reference to `r6`.
+f6(x: &r6)
+assert(r6 == 8)
+
+// In-out parameters can't have default values, and variadic parameters
+// can't be in-out.
+
+// In-out parameters are not the same as returning a value from a function.
+// In-out parameters are an alternative way for a function to have an effect
+// outside of the scope of its function body.
+
+// -----------------------------------------------------------------------------
+
+// TODO
+
+// Multiple parameters are comma-separated.
+func f6(first: Int, second: Int) {
+  // Do stuff.
+}
+
+f6(first: 1, second: 2)
+
+// -----------------------------------------------------------------------------
+
+func f7(_ first: Int, foo second: Int = 4) {
+  // Do stuff.
+}
+
+f7(1, foo: 2)
+
+
+// -----------------------------------------------------------------------------
+//  Function Types
+// -----------------------------------------------------------------------------
+
+// Every function has a specific function type made up of the parameter
+// types and the return type of the function.
+
+// func addTwoInts(a: Int, _ b: Int) -> Int {
+//   return a + b
+// }
+// func multiplyTwoInts(a: Int, _ b: Int) -> Int {
+//   return a * b
+// }
+
+
+// -----------------------------------------------------------------------------
+//  Nested Functions
+// -----------------------------------------------------------------------------
+
+func chooseStepper(reversed: Bool) -> (Int) -> Int {
+  func increase(x: Int) -> Int { return x + 1 }
+  func decrease(x: Int) -> Int { return x - 1 }
+  return reversed ? decrease : increase
+}
+
+var curValue = -4
+let stepToZero = chooseStepper(reversed: curValue > 0)
+// `stepToZero` now refers to the nested `increase()` function.
+
+while curValue != 0 {
+  print("\(curValue)... ")
+  // The label for the nested function is not allowed.
+  curValue = stepToZero(curValue)
+}
+
+print("zero!")
+
+// -4...
+// -3...
+// -2...
+// -1...
+// zero!
+
+
+// -----------------------------------------------------------------------------
+
+// // Swift 3 bug?
+//
+// func foo() -> (Int) -> Int {
+//   func bar(x: Int) -> Int { return x + 1 }
+//   return bar
+// }
+//
+// // The label for the nested function is not allowed to be used.
+//
+// print(foo()(1))
+// // 2
+//
+// let baz = foo()
+// print(baz(1))
+// // 2
--- Swift/25-advanced-operators.swift
+++ Swift/25-advanced-operators.swift
@@ -16,6 +16,8 @@ assert(a & b == 0b01001100)       // Bitwise AND.
 assert(a | b == 0b11101111)       // Bitwise OR.
 assert(a ^ b == 0b10100011)       // Bitwise XOR.
 
+// -----------------------------------------------------------------------------
+
 // Shifting unsigned integers (logical shift).
 let c: UInt8 = 0b00000100
 assert(c << 1 == 0b00001000)        // Bitwise left shift operator.
@@ -26,6 +28,8 @@ assert(c << 6 == 0b00000000)
 assert(c >> 1 == 0b00000010)        // Bitwise right shift operator.
 assert(c >> 2 == 0b00000001)
 assert(c >> 3 == 0b00000000)
+
+// -----------------------------------------------------------------------------
 
 // Shifting signed integers (arithmetic shift).
 let d: Int8 = 0b01100110
@@ -39,6 +43,8 @@ assert(e >> 2 == -0b00011010)
 assert(e >> 3 == -0b00001101)
 assert(e >> 4 == -0b00000111)
 assert(e >> 5 == -0b00000100)
+
+// -----------------------------------------------------------------------------
 
 // Getting the components of a color.
 let hotPink: UInt32 = 0xFF69B4
@@ -54,10 +60,16 @@ assert(blueComponent == 0xB4)
 //  Overflow Operators
 // -----------------------------------------------------------------------------
 
-// Overflow operators allow overflows to happens.
+// Normally, overflows are handled poorly.
 
-// var o0 = Int16.max       // 32,767 is the maximum value an Int16 can hold.
-// o0 += 1                  // Causes an error.
+// 32,767 is the maximum value an Int16 can hold.
+var o0 = Int16.max
+// The following is commented out because it causes the program to crash.
+// o0 += 1
+
+// Overflow operators allow overflows to happen.
+
+// -----------------------------------------------------------------------------
 
 // Overflow integer addition.
 var o1 = UInt8.max
@@ -65,6 +77,8 @@ o1 = o1 &+ 1
 assert(o1 == UInt8.min)
 assert(UInt8.min == 0)
 assert(UInt8.max == 255)
+
+// -----------------------------------------------------------------------------
 
 // Overflow operators don't let you know when an overflow happens
 // but methods like `addWithOverflow` do.
@@ -77,16 +91,22 @@ var o3 = Int8.addWithOverflow(o2, o2)
 assert(o3.0 == -56)
 assert(o3.overflow == true)
 
+// -----------------------------------------------------------------------------
+
 // Overflow integer subtraction.
 var o4 = UInt8.min
 o4 = o4 &- 1
 assert(o4 == UInt8.max)
+
+// -----------------------------------------------------------------------------
 
 var o5 = Int8.min
 o5 = o5 &- 1
 assert(o5 == Int8.max)
 assert(Int8.min == -128)
 assert(Int8.max == 127)
+
+// -----------------------------------------------------------------------------
 
 // Overflow integer multiplication.
 var o6: UInt8 = 4
@@ -100,7 +120,8 @@ assert(o6 == 16)
 
 let x = 2 + 3 % 4 * 5
 let y = 2 + ((3 % 4) * 5)
-assert(x == y && y == 17)
+assert(x == y)
+assert(y == 17)
 
 
 // -----------------------------------------------------------------------------
@@ -122,6 +143,8 @@ let v3 = v1 + v2
 assert(v3.x == 5.0)
 assert(v3.y == 6.0)
 
+// -----------------------------------------------------------------------------
+
 // Overload the prefix negation operator (`-`) for Vector2D values.
 prefix func - (vector: Vector2D) -> Vector2D {
   return Vector2D(x: -vector.x, y: -vector.y)
@@ -135,6 +158,8 @@ let v5 = -v4
 assert(v5.x == 5.0)
 assert(v5.y == 6.0)
 
+// -----------------------------------------------------------------------------
+
 // Overload the addition assignment operator (`+=`) for Vector2D values.
 // The `left` parameter is `inout` because our operator directly edits it.
 func += (left: inout Vector2D, right: Vector2D) {
@@ -145,6 +170,12 @@ var v6 = v1
 v6 += v2
 assert(v6.x == 5.0)
 assert(v6.y == 6.0)
+
+// It's not possible to overload the default assignment operator (`=`).
+// Compound assignment operators can be overloaded.  The ternary conditional
+// operator (`a ? b : c`) cannot be overloaded.
+
+// -----------------------------------------------------------------------------
 
 // Overload the equivalence operators (`==` and `!=`) for Vector2D values.
 func == (left: Vector2D, right: Vector2D) -> Bool {
@@ -162,6 +193,11 @@ assert(v3 != v4)
 //  Custom Operators
 // -----------------------------------------------------------------------------
 
+// The tokens `=`, `->`, `//`, `/*`, `*/`, `.`, the prefix operators `<`, `&`,
+// and `?`, the infix operator `?`, and the postfix operators `>`, `!`, and `?`
+// are reserved which means they can’t be overloaded or be used as custom
+// operators.
+
 // Define a new postfix unary operator called `+++`.
 // No associativity is defined so default to `none`.
 // No precedence is defined so default to `100`.
@@ -177,13 +213,16 @@ let v8 = v7+++
 assert(v8.x == 4.0)
 assert(v8.y == 6.0)
 
+// -----------------------------------------------------------------------------
+
 // If both a prefix operator and postfix operator are applied to the same
 // operand then the postfix operator is applied first.
-
 var v9 = v8
 v9 = -v9+++
 assert(v9.x == -8.0)
 assert(v9.y == -12.0)
+
+// -----------------------------------------------------------------------------
 
 // Define a new infix binary operator called `+-`.
 infix operator +- { associativity left precedence 140 }
@@ -195,5 +234,3 @@ func +- (left: Vector2D, right: Vector2D) -> Vector2D {
 let v10 = v7 +- v8
 assert(v10.x == 8.0)
 assert(v10.y == 0.0)
-
-// The tokens =, ->, //, /*, */, ., the prefix operators <, &, and ?, the infix operator ?, and the postfix operators >, !, and ? are reserved. These tokens can’t be overloaded, nor can they be used as custom operators.
